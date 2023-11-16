ef0535dae3e79294b4c07c1a5298cf893db0db8b
Dick Johnson <einheit@mainphrame.com>
2023-11-16 11:58:53 -0800

    Minor updates to php script and gitignore

--- getshells.php
+++ getshells.php
@@ -6,18 +6,18 @@ $shellcount = array();
 $fh = fopen("passwd", "r");
 if ($fh) {
     while (($line = fgets($fh)) !== false) {
-        // process the line read.
-        $tmp = substr($line, strrpos($line, ":") + 1);
-        if (array_key_exists($tmp, $shellcount)) {
-            $shellcount[$tmp] += 1;
-        } else
-            $shellcount[$tmp] = 1;
+	$tmp = substr($line, strrpos($line, ":") + 1);
+	if (array_key_exists($tmp, $shellcount)) {
+	    $shellcount[$tmp] += 1;
+	} else
+	    $shellcount[$tmp] = 1;
     }
     fclose($fh);
 
     foreach($shellcount as $key => $value) {
-        $key = rtrim($key);
-#        printf("%s\t%d\n", $key, $value);
+	$key = rtrim($key);
+# echo seems slightly faster than printf
+#	printf("%s\t%d\n", $key, $value);
 	echo "$key: ";
 	echo "$value\n";
     }
